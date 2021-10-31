4c2cee625d3a5aa031aefbef1f69c5c41d2067bf
Paulo Vidal <u1d4lp@gmail.com>
2021-10-31 15:48:27 -0300

    scaffold some components from reusable code and updated Campaign.sol to latest Solidity version

---
+++ ethereum/factory.ts
@@ -0,0 +1,11 @@
+// to review completely
+
+import web3 from './web3';
+import CampaignFactory from './build/CampaignFactory.json';
+
+const instance = new web3.eth.Contract(
+   JSON.parse(CampaignFactory.interface),
+   '0xa0503d65D9538dB5D7769fe9EeB1F502486FA179'
+);
+
+export default instance;
